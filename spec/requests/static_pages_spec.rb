7c11d80a7bb23257ed790207a02b2ff13699163a
Luthan T. Hill <luthanhill3@gmail.com>
2014-06-18 00:13:34 -0500

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,21 +8,41 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Shopalite')
     end
-  end
 
-	describe "About page" do
+  	it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Shopalite")
+    end
 
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About')
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('Home -')
     end
   end
 
   describe "Contact page" do
 
-    it "should have the content 'Contact'" do
+    it "should have the content 'Contact Us'" do
       visit '/static_pages/contact'
       expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Contact Us - Shopalite")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About - Shopalite")
     end
   end
 end
