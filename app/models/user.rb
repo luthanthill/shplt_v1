c3b61e6ac4618747fda92d6d99c79b6dffeca955
Luthan T. Hill <luthanhill3@gmail.com>
2014-07-03 00:16:45 -0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
+	before_save { username.downcase! }
 	before_save { email.downcase! }
 	validates :first_name, length: { maximum: 20 }
 	validates :last_name, length: { maximum: 20 }
-	validates :username, presence: true, length: { maximum: 30 }
+	validates :username, presence: true, length: { maximum: 30 }, uniqueness: { case_sensitive: false }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
